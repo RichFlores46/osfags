ea31c52a1278e6c27278d91b80bc445a2a9a2ef9
RichFlores46 <117998174+RichFlores46@users.noreply.github.com>
2023-05-23 13:40:55 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,20 +2,22 @@ const STATIC_CACHE = "static";
 //Se declara un vector con la ubicacion y el nombre
 const APP_SHELL =[
 "/",
-"index.html",
-"css/styles.css",
-"js/main.js",
-"js/arriba.js",
-"img/logo_osfags.png",
-"Transparencia/index.html",
-"Sobre-el-Órgano/index.html",
-"Opiniones/index.html",
-"Denuncias/index.html",
-"Datos-Personales/index.html",
-"Comité/index.html",
-"assets/casa-corazón.mp4",
-"assets/hogar-de-la-niña-web.mp4",
-"img/col_cap,pr.jpg"
+"/index.html",
+"/css/styles.css",
+"/js/main.js",
+"/js/arriba.js",
+"/img/logo_osfags.png",
+"/Transparencia/index.html",
+"/Sobre-el-Órgano/index.html",
+"/Opiniones/index.html",
+"/Denuncias/index.html",
+"/Datos-Personales/index.html",
+"/Comité/index.html",
+"/assets/casa-corazón.mp4",
+"/assets/hogar-de-la-niña-web.mp4",
+"/img/col_cap,pr.jpg",
+"https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css",
+"https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
 ];
 //En el evento install se agregan los archivos a la 
 self.addEventListener("install", (e) => {
